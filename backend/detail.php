5bcd6fe551096039ec9713e841a3b3d89002e4f9
yuuhei-koutoku <yhkt.0627@gmail.com>
2023-03-24 06:31:24 +0900

    delete session of article detail

--- backend/detail.php
+++ backend/detail.php
@@ -6,21 +6,17 @@ require_once dirname(__FILE__) . '/Bootstrap.class.php';
 
 use koujigenba_php\backend\Bootstrap;
 use koujigenba_php\backend\lib\PDODatabase;
-use koujigenba_php\backend\lib\Session;
 use koujigenba_php\backend\lib\Article;
 
 $db = new PDODatabase(Bootstrap::DB_HOST, Bootstrap::DB_USER, Bootstrap::DB_PASS, Bootstrap::DB_NAME, Bootstrap::DB_TYPE);
-$session = new Session($db);
 
 $loader = new \Twig_Loader_Filesystem(Bootstrap::TEMPLATE_DIR);
 $twig = new \Twig_Environment($loader, [
     'cache' => Bootstrap::CACHE_DIR
 ]);
 
-$session->checkSession();
-
 if ($_GET === []) {
-    // セッションがなければ、トップページへリダイレクト
+    // $_GETのパラメーターがなければ、トップページへリダイレクト
     header('Location: ' . Bootstrap::ENTRY_URL);
 } else {
     $article = new Article($db);
