3da5c73e5969a4d35b1d511ceffdaa0e7ac0c728
yuuhei-koutoku <yhkt.0627@gmail.com>
2023-03-24 14:49:02 +0900

    add edit and delete buttons to detail

--- backend/detail.php
+++ backend/detail.php
@@ -6,14 +6,20 @@ require_once dirname(__FILE__) . '/Bootstrap.class.php';
 
 use koujigenba_php\backend\Bootstrap;
 use koujigenba_php\backend\lib\PDODatabase;
+use koujigenba_php\backend\lib\Session;
 use koujigenba_php\backend\lib\Article;
 
 $db = new PDODatabase(Bootstrap::DB_HOST, Bootstrap::DB_USER, Bootstrap::DB_PASS, Bootstrap::DB_NAME, Bootstrap::DB_TYPE);
+$session = new Session($db);
 
 $loader = new \Twig_Loader_Filesystem(Bootstrap::TEMPLATE_DIR);
 $twig = new \Twig_Environment($loader, [
     'cache' => Bootstrap::CACHE_DIR
 ]);
+
+$session->checkSession();
+
+$template = 'detail.html.twig';
 
 if ($_GET === []) {
     // $_GETのパラメーターがなければ、トップページへリダイレクト
@@ -22,12 +28,13 @@ if ($_GET === []) {
     $article = new Article($db);
     // article_idをもとに、記事詳細情報を取得
     $detailArr = $article->getArticle($_GET['article_id']);
-}
 
-$template = 'detail.html.twig';
+    // 記事削除処理
+    require_once './article/delete.php';
+}
 
 $context = [];
 $context['article']['detailArr'] = $detailArr[0];
-
+$context['session'] = $_SESSION;
 $template = $twig->loadTemplate($template);
 $template->display($context);
